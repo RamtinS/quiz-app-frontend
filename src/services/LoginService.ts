bd316c83049e216732524789eef816df9fbead89
Tobias <oftedal03@gmail.com>
2024-03-22 15:45:05 +0100

    Implement login service

---
+++ src/services/LoginService.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import type {LoginRequestDTO} from "@/models/LoginRequestDTO";
+import type {LoginResponseDTO} from "@/models/LoginResponseDTO";
+import type { AxiosResponse } from 'axios';
+export class LoginService {
+    private readonly api_url = "http://localhost:8080/api/v1"
+
+    async login(loginRequest: LoginRequestDTO) {
+        try {
+            console.log('Trying to log in with u:' + loginRequest.username + ' pw:' + loginRequest.password);
+
+            const result: AxiosResponse<LoginResponseDTO> = await axios.post(this.api_url + "/login", loginRequest);
+            if (result.status === 200){
+                console.log("token is " + result.data.token)
+                return result.data;
+            }
+        } catch (err) {
+            console.log('error: ' + err);
+            throw err;
+        }
+    }
+}
