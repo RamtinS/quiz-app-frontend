8362a46100cf121a26b2d563a7b44db4b73d7ab5
Tobias <oftedal03@gmail.com>
2024-03-22 15:49:45 +0100

    Create not found page for 404 errors

---
+++ src/components/navigation/NotFound.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div >
+    <div id="error-message">
+      <h1>
+        Error 404: Page has not been found
+      </h1>
+      <p id="error-info">
+        We cannot find the page. Are you sure the URL is correct?
+      </p>
+      <img id="detective" src="@/assets/images/detective.png" alt="detective looking for your page">
+      <p id="running-url">
+        Your URL
+      </p>
+    </div>
+  </div>
+
+
+</template>
+
+<style scoped>
+
+h1{
+  width: 100%;
+  margin: 0;
+  padding-top: 10px;
+
+}
+p{
+  margin: 0;
+  padding: 0;
+}
+
+#error-message{
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  text-align: center;
+}
+
+
+
+#running-url{
+  animation: move-around 10s linear infinite;
+
+}
+
+#error-info{
+  padding-top: 5px;
+  padding-bottom: 30px;
+}
+
+#detective{
+  width: 200px;
+  margin-left:auto;
+  margin-right:auto;
+  filter: drop-shadow( 0 0 25px #FC0) drop-shadow( 0 0 50px green);
+  animation: search-around 10s ease-in-out infinite;
+}
+
+@keyframes move-around {
+
+  25%{
+    transform: translate(250px) rotate(-180deg);
+  }
+  75%{
+    transform: translateX(-250px) rotate(180deg);
+  }
+
+}
+
+@keyframes search-around {
+
+  25%{
+    transform: translate(40px);
+  }
+  75%{
+    transform: translateX(-40px) ;
+  }
+
+}
+</style>
