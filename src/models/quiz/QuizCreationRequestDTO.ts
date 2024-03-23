99abb2461a2f1807467956323e39621b9d56766a
Tobias <oftedal03@gmail.com>
2024-03-23 15:29:32 +0100

    Create QuizCreationRequestDTO.ts

---
+++ src/models/quiz/QuizCreationRequestDTO.ts
@@ -0,0 +1,7 @@
+import type {QuizQuestionDTO} from "@/models/quiz/QuizQuestionDTO";
+
+export type QuizCreationRequestDTO = {
+    title: string;
+    description: string;
+    questions: QuizQuestionDTO[];
+}
