d0aa56c9fa3d29e50f2e86cfcc234968b2f142a3
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-03-25 15:23:36 +0100

    Refactor LoginService to handle unexpected status codes and improve exception handling

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -1,23 +1,23 @@
 import axios from 'axios';
-
 import type { AxiosResponse } from 'axios';
 import type {LoginRequestDTO} from "@/models/user/LoginRequestDTO";
 import type {LoginResponseDTO} from "@/models/user/LoginResponseDTO";
+
 export class LoginService {
-    private readonly api_url = "http://localhost:8080/api/v1/auth"
+  private readonly api_url = "http://localhost:8080/api/v1/auth"
 
-    async login(loginRequest: LoginRequestDTO) {
-        try {
-            console.log('Trying to log in with u:' + loginRequest.username + ' pw:' + loginRequest.password);
+  async login(loginRequest: LoginRequestDTO) : Promise<LoginResponseDTO> {
+    try {
+      const response: AxiosResponse<LoginResponseDTO> = await axios.post(this.api_url + "/login", loginRequest);
 
-            const result: AxiosResponse<LoginResponseDTO> = await axios.post(this.api_url + "/login", loginRequest);
-            if (result.status === 200) {
-                console.log("token is " + result.data.token)
-                return result.data;
-            }
-        } catch (err) {
-            console.log('error: ' + err);
-            throw err;
-        }
+      if (response.status === 200) {
+        return response.data;
+      } else {
+        throw new Error(`Unexpected status code: ${response.status}`);
+      }
+
+    } catch (err) {
+      throw err;
     }
+  }
 }
