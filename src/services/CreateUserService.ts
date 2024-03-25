91d7b174f3c7a159a6a268e6cd01ec090a58aa6f
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-03-25 15:26:38 +0100

    Refactor createUser() in CreateUserService to handle unexpected status codes

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,28 +1,34 @@
 import axios from 'axios';
+import type { AxiosResponse } from 'axios';
 import type {CreateUserRequestDTO} from "@/models/user/CreateUserRequestDTO";
 import type {CreateUserResponseDTO} from "@/models/user/CreateUserResponseDTO";
 import type {EditUserDTO} from "@/models/user/EditUserDTO";
 
 export class CreateUserService {
-    private readonly api_url = "http://localhost:8080/api/v1/auth"
+  private readonly api_url = "http://localhost:8080/api/v1/auth"
 
-    async createUser(createUserRequestDTO: CreateUserRequestDTO): Promise<CreateUserResponseDTO> {
-        try {
-            const response = await axios.post(this.api_url + "/register", createUserRequestDTO);
-            return response.data.token;
-        } catch (err) {
-            console.log('error: ' + err);
-            throw err;
-        }
-    }
+  async createUser(createUserRequestDTO: CreateUserRequestDTO): Promise<CreateUserResponseDTO> {
+    try {
+      const response: AxiosResponse<CreateUserResponseDTO> = await axios.post(this.api_url + "/register", createUserRequestDTO);
 
-    async editUser(newInformation: EditUserDTO): Promise<CreateUserResponseDTO> {
-        try {
-            const response = await axios.put(this.api_url + "/edit", newInformation);
-            return response.data;
-        } catch (err) {
-            console.log('error: ' + err);
-            throw err;
-        }
+      if (response.status === 201) {
+        return response.data;
+      } else {
+        throw new Error(`Unexpected status code: ${response.status}`);
+      }
+
+    } catch (err) {
+      throw err;
     }
+  }
+
+  async editUser(newInformation: EditUserDTO): Promise<CreateUserResponseDTO> {
+    try {
+      const response = await axios.put(this.api_url + "/edit", newInformation);
+      return response.data;
+    } catch (err) {
+      console.log('error: ' + err);
+      throw err;
+    }
+  }
 }
