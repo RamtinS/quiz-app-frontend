6f801d8a8457cb77813e90359c5a5649779389d8
Elementum <tabirijeffrey@gmail.com>
2024-03-29 16:24:30 +0100

    refactor(Login): refactor design to login

--- src/components/user/UserLogin.vue
+++ src/components/user/UserLogin.vue
@@ -44,164 +44,152 @@ async function login() {
 
 
 <template>
+  <div class="flex">
+    <div class="grid-container">
 
-  <div id="rectangle-container">
-    <div id="welcome-message">
-      <h1>
-        Welcome to Quiz App
-      </h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet blanditiis deserunt dolores eos esse et
-        incidunt ipsa labore nisi odit optio porro quae quaerat, quam quibusdam repellendus sint ut?
-      </p>
+      <div class="flex-container-1">
+        <div class="item-1">
+          <h1>Welcome!</h1>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum nihil placeat possimus quasi quisquam tempora
+          veniam. Accusantium, atque beatae blanditiis consectetur culpa ducimus labore laboriosam nulla officiis
+          ratione repellendus, voluptates!
+        </p>
+      </div>
 
-    </div>
-    <div id="login-form">
+      <div class="flex-container-2">
 
-      <h2>User Login</h2>
-
-      <form @submit.prevent="login">
-
-        <div>
-          <span class="material-icons" title="Enter a valid username">person</span>
-          <input type="text" v-model="username" placeholder="Enter your username" required/>
+        <div class="item-1">
+          <h1>Log in!</h1>
         </div>
 
-        <div>
-          <span class="material-icons" title="Enter a valid username">lock</span>
-          <input type="password" v-model="password" placeholder="Enter your password" required/>
-        </div>
+        <div class="form-container-1">
+          <form action="">
+            <label for="fusername">Username:</label><br>
+            <input class="input" type="text" id="fusername" v-model="username" required/><br>
 
-        <button type="submit">Log in</button>
+            <label for="fpassword">Password:</label><br>
+            <input class="input" type="password" id="fpassword" v-model="password" name="fpassword" required/><br>
+
+            <input type="submit" value="Login"/>
+          </form>
+        </div>
 
         <p>
-          Don't have an account? <router-link to="/register-user">Register</router-link>
+          <router-link to="/register-user">Create your account --></router-link>
         </p>
 
-      </form>
+      </div>
 
-      <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
+      <!--
 
-    </div>
+
+     <div class="form-container">
+       <form @submit.prevent="login">
+         <label for="fusername">Username:</label>
+         <input type="text" v-model="username" name="fusername" required/>
+
+         <label for="fpassword">Password:</label>
+         <input type="password" v-model="password" name="fpassword" required/>
+
+         <input type="submit" value="Login"/>
+       </form>
+     </div>
+
+       <p>
+         <router-link to="/register-user">Create your account</router-link>
+        </p>
+
+        <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
+      -->
+      </div>
+
   </div>
 
 
 </template>
 
 <style scoped>
-
-#rectangle-container {
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+.flex {
   display: flex;
-  margin-left: 10%;
-  margin-right: 10%;
-  margin-top: 5%;
-  height: 700px;
-  border-radius: 20px;
-  box-shadow: grey 0 0 20px 10px;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  font-family: "Inter", sans-serif;
 }
 
-@media (max-width: 600px){
-  #rectangle-container {
-    flex-direction: column;
-    height: auto;
-    margin: 0;
-    border-radius: 0;
-  }
-  #welcome-message, #login-form{
-    width: 100%;
-  }
-  #welcome-message{
-    background: linear-gradient(180deg, var(--primary-light) 0%, var(--secondary) 50%, var(--secondary-light) 100%);
-  }
-}
-
-@media (min-width: 600px){
-  #welcome-message, #login-form{
-    width: 50%;
-  }
-  #welcome-message{
-    background: linear-gradient(90deg, var(--primary-light) 0%, var(--secondary) 50%, var(--secondary-light) 100%);
-  }
-}
-
-p, h1, h2{
-  color: white;
-  text-align: center;
-  text-shadow: black 0 0 15px;
-}
-
-h1 {
-  font-size: 3em;
-}
-
-p {
-  padding: 0 10%;
-}
-
-#welcome-message {
-  padding-top: 50px;
-  border-top-left-radius: inherit;
-  border-bottom-left-radius: inherit;
-}
-
-#login-form {
-  padding-top: 50px;
-  border-top-right-radius: inherit;
-  border-bottom-right-radius: inherit;
+.flex-container-1 {
   display: flex;
   flex-direction: column;
+  justify-content: center;
   text-align: center;
-  background-color: var(--secondary-light);
-  h2{
-    color: white;
-    font-size: 2em;
-  }
+  padding: 10%;
+}
 
-  form {
-    padding-top: 50px;
+.flex-container-2 {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 10%;
+}
 
-    border-radius: inherit;
+.item-1 {
+  text-align: center;
+}
 
-    display: flex;
-    flex-direction: column;
-    overflow: hidden;
+input[type=submit] {
+  font-weight: bold;
+  width: 100%;
+  background-color: #242062;
+  color: white;
+  padding: 14px 20px;
 
-    div, button {
-      width: 50%;
-      max-width: 300px;
-      text-align: center;
-      margin: auto auto 20px;
-      border-radius: 10px;
-      height: 40px;
-      box-shadow: grey 5px 5px 5px;
-    }
+  border: none;
+  border-radius: 15px;
+  cursor: pointer;
+  font-size: 16px;
+}
 
-    div {
-      display: flex;
-      background-color: white;
-      align-items: center;
-      :hover {
-        cursor: pointer;
-      }
+.input {
+  font-weight: bold;
+  padding: 14px 20px;
+  border-radius: 15px;
+  margin-bottom: 35px;
+  margin-top: 10px;
+}
 
-      input {
-        outline: none;
-        border-radius: inherit;
+input[type="text"] {
+  border: 2px solid rgba(0, 0, 0, 0.17)
+}
 
-        width: 100%;
-        box-sizing: border-box;
-        border: none;
-        height: 90%;
-      }
-      span{
-        display: flex;
-      }
-    }
-    button:hover {
-      cursor: pointer;
-      scale: 1.05;
-    }
-  }
+input[type="password"] {
+  border: 2px solid rgba(0, 0, 0, 0.17)
+}
+
+
+
+
+label {
+  font-weight: bold;
+}
+
+.grid-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  border-radius: 20px;
+
+  width: 1080px;
+  height: 600px;
+  background: rgba(255, 255, 255, 0.96);
+}
+
+
+
+label {
+  margin-top: 10px;
 }
 
 .error-message {
