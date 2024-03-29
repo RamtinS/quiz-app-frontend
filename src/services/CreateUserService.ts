63976fd75fb82c03501b3a584659f0cfc72bc7b0
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-03-29 15:31:50 +0100

    Remove editUser() from CreateUserService

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import type { AxiosResponse } from 'axios';
 import type {CreateUserRequestDTO} from "@/models/user/CreateUserRequestDTO";
 import type {CreateUserResponseDTO} from "@/models/user/CreateUserResponseDTO";
-import type {EditUserDTO} from "@/models/user/EditUserDTO";
 
 export class CreateUserService {
   private readonly api_url = "http://localhost:8080/api/v1/auth"
@@ -18,16 +17,6 @@ export class CreateUserService {
       }
 
     } catch (err) {
-      throw err;
-    }
-  }
-
-  async editUser(newInformation: EditUserDTO): Promise<CreateUserResponseDTO> {
-    try {
-      const response = await axios.put(this.api_url + "/edit", newInformation);
-      return response.data;
-    } catch (err) {
-      console.log('error: ' + err);
       throw err;
     }
   }
