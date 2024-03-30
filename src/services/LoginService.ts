1ac04be78113bb30525a822cc4215c870a337a67
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-03-30 14:33:53 +0100

    Refactor LoginService: Make API URL and login() static

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -4,9 +4,9 @@ import type {LoginRequestDTO} from "@/models/user/LoginRequestDTO";
 import type {LoginResponseDTO} from "@/models/user/LoginResponseDTO";
 
 export class LoginService {
-  private readonly api_url = "http://localhost:8080/api/v1/auth"
+  private static readonly api_url = "http://localhost:8080/api/v1/auth"
 
-  async login(loginRequest: LoginRequestDTO) : Promise<LoginResponseDTO> {
+  public static async login(loginRequest: LoginRequestDTO) : Promise<LoginResponseDTO> {
     try {
       const response: AxiosResponse<LoginResponseDTO> = await axios.post(this.api_url + "/login", loginRequest);
 
