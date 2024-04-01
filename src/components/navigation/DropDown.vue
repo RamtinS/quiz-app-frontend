17070b5335066b7c5e125ad387ec76d7e7ecb4e3
Tobias <oftedal03@gmail.com>
2024-04-01 09:42:13 +0200

    Create DropDown.vue

---
+++ src/components/navigation/DropDown.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+
+const props = defineProps(
+    {
+      left: {type: Boolean, required: false, default: false},
+    }
+)
+
+
+
+</script>
+
+<template>
+
+  <div
+      id="dropdown"
+      :class="[left ? 'left' : 'right']"
+  >
+    <slot>
+    </slot>
+  </div>
+</template>
+
+<style scoped>
+
+
+
+#dropdown {
+  position: absolute;
+  top: 100%;
+  height: auto;
+
+}
+
+.left {
+  left: 0;
+}
+
+.right {
+  right: 0;
+}
+
+
+
+
+</style>
