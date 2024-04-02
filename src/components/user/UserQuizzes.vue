a0462e085295c69d609cd9489cf1339c05c0f6e5
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-03 01:49:20 +0200

    Create UserQuizzes component to manage and display user quizzes

---
+++ src/components/user/UserQuizzes.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+import QuizBrowser from "@/components/quiz/QuizBrowser.vue";
+import {useUserStore} from "@/stores/UserStore";
+import UserProfileHeader from "@/components/user/UserProfileHeader.vue";
+
+const userStore = useUserStore();
+
+
+
+
+</script>
+
+<template>
+
+  <div id="user-quizzes">
+
+    <UserProfileHeader></UserProfileHeader>
+
+    <QuizBrowser :title="'All user quizzes'" :username="userStore.getUserData('username')"></QuizBrowser>
+
+
+  </div>
+
+</template>
+
+<style scoped>
+
+#user-quizzes {
+  padding: 2%;
+}
+
+#user-quizzes {
+  flex-grow: 1;
+  max-height: 100vh;
+  overflow-y: auto;
+}
+
+</style>
