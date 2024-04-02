d532284e24c2544ec14d95fb32d066bd7a190423
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-03 01:52:11 +0200

    Remove redundant CSS form UserProgress.vue

--- src/components/user/UserProgress.vue
+++ src/components/user/UserProgress.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 
 import UserProfileHeader from "@/components/user/UserProfileHeader.vue";
+
 </script>
 
 <template>
-
   <div id="user-progress">
 
     <UserProfileHeader></UserProfileHeader>
 
   </div>
-
 </template>
 
 <style scoped>
@@ -18,34 +17,6 @@ import UserProfileHeader from "@/components/user/UserProfileHeader.vue";
 #user-progress {
   padding: 2%;
 }
-
-nav {
-  text-align: left;
-  width: 100%;
-  margin-top: 1rem;
-}
-
-nav a {
-  color: var(--primary);
-}
-
-nav a.router-link-exact-active {
-  color: grey;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-#separator {
-  border: 1px solid var(--primary);
-}
-
 
 
 </style>
