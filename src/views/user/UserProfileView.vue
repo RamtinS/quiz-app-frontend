3f4cb42f6ebea1e5c9999e2c732a7d9fb343e0aa
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-03 01:39:19 +0200

    Add new views: UserProfileView, UserProgressView, UserQuizzesView and UserLoginView

---
+++ src/views/user/UserProfileView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import UserProfile from "@/components/user/UserProfile.vue";
+</script>
+
+<template>
+  <div>
+    <UserProfile></UserProfile>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
