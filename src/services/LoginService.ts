98a3a994417de46e7a7ae38d83ddefd0ff338636
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-02 17:33:02 +0200

    Add ErrorResponseDTO in return type for login() in LoginService

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -2,22 +2,18 @@ import axios from 'axios';
 import type { AxiosResponse } from 'axios';
 import type {LoginRequestDTO} from "@/models/user/LoginRequestDTO";
 import type {LoginResponseDTO} from "@/models/user/LoginResponseDTO";
+import type {ErrorResponseDTO} from "@/models/ErrorResponseDTO";
 
 export class LoginService {
   private static readonly api_url = "http://localhost:8080/api/v1/auth"
 
-  public static async login(loginRequest: LoginRequestDTO) : Promise<LoginResponseDTO> {
+  public static async login(loginRequest: LoginRequestDTO) : Promise<LoginResponseDTO | ErrorResponseDTO> {
     try {
       const response: AxiosResponse<LoginResponseDTO> = await axios.post(this.api_url + "/login", loginRequest);
-
-      if (response.status === 200) {
-        return response.data;
-      } else {
-        throw new Error(`Unexpected status code: ${response.status}`);
-      }
-
+      return response.data;
     } catch (err) {
       throw err;
     }
   }
 }
+
