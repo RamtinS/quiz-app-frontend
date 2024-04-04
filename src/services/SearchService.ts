303c6d5fd314b78485f526ea593ec0d769e938aa
Elementum <tabirijeffrey@gmail.com>
2024-04-04 18:08:29 +0200

    feat(SearchService): redesign search service

--- src/services/SearchService.ts
+++ src/services/SearchService.ts
@@ -6,15 +6,30 @@ import type {QuizPreviewDTO} from "@/models/quiz/QuizPreviewDTO";
 
 export class SearchService {
 
-    private static readonly api_url = "http://localhost:8080/api/v1/quiz-management"
+    private static readonly quiz_api_url = "http://localhost:8080/api/v1/quiz-management"
+    private static readonly user_api_url = "http://localhost:8080/api/v1/users"
 
     public static async searchQuizzesByName(name: string, page: number,
-                                             pageSize: number): Promise<QuizPreviewDTO> {
+                                             pageSize: number): Promise<QuizPreviewDTO[]> {
 
-        const url = this.api_url + "?" + "search=" + name + "&page=" + page + "&size=" + pageSize;
+        const url = this.quiz_api_url + "?" + "search=" + name + "&page=" + page + "&size=" + pageSize;
         console.log("url: " + url);
         try {
-            const response: AxiosResponse<QuizPreviewDTO> = await axios.get(url);
+            const response: AxiosResponse<QuizPreviewDTO[]> = await axios.get(url);
+            return response.data;
+        } catch (err) {
+            console.log('error in search service: ' + err);
+            throw err;
+        }
+    }
+
+    public static async searchUserByUsername(username: string, page: number,
+                                             pageSize: number): Promise<PublicUserInformationDTO[]> {
+
+        const url = this.user_api_url + "?" + "search=" + username + "&page=" + page + "&size=" + pageSize;
+        console.log("url: " + url);
+        try {
+            const response: AxiosResponse<PublicUserInformationDTO[]> = await axios.get(url);
             return response.data;
         } catch (err) {
             console.log('error in search service: ' + err);
