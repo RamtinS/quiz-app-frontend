1c361c337f469e61df5355d973f3251baf0586da
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-04 14:10:09 +0200

    Update API url in QuizAttemptService

--- src/services/QuizAttemptService.ts
+++ src/services/QuizAttemptService.ts
@@ -1,16 +1,17 @@
 import axios from "axios";
+import type { AxiosResponse } from 'axios';
 import type { QuizAttemptDTO } from "@/models/quiz/QuizAttemptDTO";
+import type { QuizProgressDTO } from "@/models/quiz/QuizProgressDTO";
 
 export class QuizAttemptService {
-    //TODO api must be fixed.
-    private static readonly api_url = "http://localhost:8080/api/v1/feedback"
 
-    public static async sendQuizAttempt(quizAttemptDTO: QuizAttemptDTO): Promise<void> {
-        try {
-            await axios.post(this.api_url + "/submit", quizAttemptDTO);
-        } catch (err) {
-            throw err;
-        }
+  private static readonly api_url = "http://localhost:8080/api/v1/quiz-attempt"
+
+  public static async sendQuizAttempt(quizAttemptDTO: QuizAttemptDTO): Promise<void> {
+    try {
+      await axios.post(this.api_url, quizAttemptDTO);
+    } catch (err) {
+      throw err;
     }
-
+  }
 }
