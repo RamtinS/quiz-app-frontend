aaa679c2a6b930a4a3167e00fdfb9bf59a2375c5
Tobias <oftedal03@gmail.com>
2024-04-05 11:51:42 +0200

    remove type from QuizCreationRequestDTO.ts

--- src/models/quiz/QuizCreationRequestDTO.ts
+++ src/models/quiz/QuizCreationRequestDTO.ts
@@ -2,7 +2,6 @@ import type {QuizQuestionDTO} from "@/models/quiz/QuizQuestionDTO";
 import type {TagDTO} from "@/models/quiz/TagDTO";
 
 export type QuizCreationRequestDTO = {
-    type: string;
     title: string;
     description: string;
     categoryDescription: string;
