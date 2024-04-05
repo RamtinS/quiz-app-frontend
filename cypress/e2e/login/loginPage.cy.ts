55f9d975f8e27b9083129ee9263230bcb7a7ee44
Elementum <tabirijeffrey@gmail.com>
2024-04-05 18:46:02 +0200

    test(Login): add Cypress tests

---
+++ cypress/e2e/login/loginPage.cy.ts
@@ -0,0 +1,37 @@
+describe('Login page test', () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000/login")
+    });
+
+    it("Button should be deactivated on empty input.", () => {
+        cy.get('#username').type(' ');
+        cy.get('#password').type(' ');
+        cy.get('#login').should('be.disabled');
+    });
+
+    it("Button should be activated on filled input.", () => {
+        cy.get('#username').type('Text');
+        cy.get('#password').type('Text');
+        cy.get('#login').should('not.be.disabled');
+    });
+
+    it("Display error on incorrect input.", () => {
+        cy.get('#username').type('Text');
+        cy.get('#password').type('Text');
+        cy.get('#login').click()
+        cy.get("#error").should('not.be.hidden')
+    });
+
+    it("Display homeView on correct input.", () => {
+        cy.get('#username').type('admin');
+        cy.get('#password').type('password');
+        cy.get('#login').click()
+        cy.get("#welcome").should('not.be.hidden')
+    });
+
+    it("Create-Account button pushes you to a new route.", () => {
+        cy.get('#register-link').click()
+        cy.get("#register-button").should("be.visible");
+    });
+})
+
