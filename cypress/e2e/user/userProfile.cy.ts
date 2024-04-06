404e43959d20eb5730c7bca8b52fce50fdbc90d8
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-07 00:07:53 +0200

    Create e2e test for UserProfile

---
+++ cypress/e2e/user/userProfile.cy.ts
@@ -0,0 +1,19 @@
+
+/**
+ * Cypress test for testing the UserProfile component.
+ */
+describe('User Profile Component', () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/user-profile");
+    cy.login('http://localhost:3000/login?redirect=/user-profile', 'TestUser', 'password');
+  });
+
+  it('displays user profile information', () => {
+
+    cy.get('[data-cy="profile-info"]').should('exist');
+
+    cy.get('[data-cy="first-name"]').should('contain', 'Test');
+    cy.get('[data-cy="surname"]').should('contain', 'Test');
+    cy.get('[data-cy="email"]').should('contain', 'test@test.no');
+  });
+});
