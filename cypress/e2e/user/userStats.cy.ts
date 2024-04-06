69c30dbde0c6c1a271d4c95e4ecc1303bcd61a34
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-06 23:06:14 +0200

    Create e2e test for UserStats

---
+++ cypress/e2e/user/userStats.cy.ts
@@ -0,0 +1,40 @@
+
+/**
+ * Cypress test for testing the UserStats component.
+ */
+describe('User stats page test', () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/user-stats");
+    cy.loginStats('admin', 'password');
+  });
+
+  it('displays user progress graph and stats', () => {
+
+    cy.get('[data-cy="graph"]').should('exist');
+
+    cy.get('[data-cy="y-axis"]').should('exist');
+
+    cy.get('[data-cy="x-axis"]').should('exist');
+
+    cy.get('[data-cy="y-labels"] text').should('have.length', 15);
+
+    cy.get('[data-cy="x-labels"] text').should('have.length', 7);
+
+    cy.get('[data-cy="quiz-point"]').should('have.length.gt', 0);
+
+    cy.get('[data-cy="graph-line"]').should('have.length.gt', 0);
+
+    cy.get('[data-cy="stats-container"]').should('exist');
+  });
+
+  it('displays error message on data fetch failure', () => {
+
+    cy.intercept('GET', 'http://localhost:8080/api/v1/users/stats', { statusCode: 500 });
+
+    cy.get('[data-cy="error-message"]').should('exist');
+
+    cy.get('[data-cy="error-message"]').should('contain',
+      'An error occurred while retrieving the statistics. Please try again later.');
+  });
+});
+
