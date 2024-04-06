4269c0c0f11160d2aea451f84477593348acb2dd
Tobias <oftedal03@gmail.com>
2024-04-06 20:23:08 +0200

    Add logout to ExpiredTokenService.ts

--- src/services/ExpiredTokenService.ts
+++ src/services/ExpiredTokenService.ts
@@ -1,10 +1,16 @@
 import router from "@/router";
+import {useUserStore} from "@/stores/UserStore";
 
+/**
+ * Service for handling expired tokens.
+ */
 export class ExpiredTokenService {
   /**
    * Pushes the user to the login page with a message that their token has expired.
    */
   public static async refreshAccessToken() {
+    const store = useUserStore();
+    store.logout();
     await router.push({
       name: 'login',
       params: { tokenStatus: "expired" },
