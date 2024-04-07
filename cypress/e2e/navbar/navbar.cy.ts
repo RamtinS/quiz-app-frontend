d2a176c4b52238ff8735ba4570d8186df8ab7c1a
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-07 12:36:01 +0200

    Refactor navbar.cy.ts to use data-cy

--- cypress/e2e/navbar/navbar.cy.ts
+++ cypress/e2e/navbar/navbar.cy.ts
@@ -1,58 +1,21 @@
+/**
+ * Cypress test for testing NavigationBar component.
+ */
 describe('Navigate website page test', () => {
 
-    //Open the right and left navigation bars.
     beforeEach(() => {
-        cy.visit("http://localhost:3000")
-        cy.get('#hamburger-icon').click()
-        cy.get('#right-preview').click()
+        cy.visit("http://localhost:3000");
     });
 
-    it("Buttons in left navbar should be visible.", () => {
-        cy.get("#login-link")
-        cy.get("#register-link")
-        cy.get("#browser-link")
-        cy.get("#contact-link")
-        cy.get("#creator-link")
+    it('displays search bar', () => {
+        cy.get('[data-cy="search-bar"]').should('exist');
     });
 
-    it("Buttons in right navbar should be visible.", () => {
-        cy.get("#user-profile")
-        cy.get("#log-out")
+    it('Displays hamburger sidebar', () => {
+        cy.get('[data-cy="hamburger-sidebar"]').should('exist');
     });
 
-    it("Login button should route to login page.", () => {
-        cy.get("#login-link").click()
-        cy.url().should('include', '/login')
+    it('Displays my account preview', () => {
+        cy.get('[data-cy="my-account-preview"]').should('exist');
     });
-
-    it("Register button should route to register-user page.", () => {
-        cy.get("#register-link").click()
-        cy.url().should('include', '/register-user')
-    });
-
-    it("Browser button should route to quiz browser page.", () => {
-        cy.get("#browser-link").click()
-        cy.url().should('include', '/quiz-browser')
-    });
-
-    it("Contact button should route to quiz browser page.", () => {
-        cy.get("#contact-link").click()
-        cy.url().should('include', '/contact')
-    });
-
-    it("Quiz creator button should route to quiz browser page.", () => {
-        cy.get("#browser-link").click()
-        cy.url().should('include', '/quiz-browser')
-    });
-
-    it("Quiz creator button should route to quiz browser page.", () => {
-        cy.get("#browser-link").click()
-        cy.url().should('include', '/quiz-browser')
-    });
-
-    it("Log-out button should route to quiz browser page.", () => {
-        cy.get("#log-out").click()
-        cy.url().should('include', '/logout')
-    });
-
 })
