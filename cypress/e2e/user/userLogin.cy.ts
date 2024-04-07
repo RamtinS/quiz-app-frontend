d87ae280f1960151b5e7713c99f242fed2fc2d5b
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-04-07 14:43:10 +0200

    Fix typo in userLogin.cy.ts

--- cypress/e2e/user/userLogin.cy.ts
+++ cypress/e2e/user/userLogin.cy.ts
@@ -14,25 +14,25 @@ describe('Login page test', () => {
 
     it('allows user to log in with valid credentials', () => {
         cy.get('[data-cy=username-input]').type('TestUser');
-        cy.get('[data-cy=username-input]').type('password');
+        cy.get('[data-cy=password-input]').type('password');
         cy.get('[data-cy=login-button]').click();
     });
 
     it("Button should be deactivated on empty input.", () => {
         cy.get('[data-cy=username-input]').clear();
-        cy.get('[data-cy=username-input]').clear();
+        cy.get('[data-cy=password-input]').clear();
         cy.get('[data-cy=login-button]').should('be.disabled');
     });
 
     it("Button should be activated on filled input.", () => {
         cy.get('[data-cy=username-input]').type('Text');
-        cy.get('[data-cy=username-input]').type('Text');
+        cy.get('[data-cy=password-input]').type('Text');
         cy.get('[data-cy=login-button]').should('not.be.disabled');
     });
 
     it("Display error on incorrect input.", () => {
         cy.get('[data-cy=username-input]').type('tes');
-        cy.get('[data-cy=username-input]').type('test');
+        cy.get('[data-cy=password-input]').type('test');
         cy.get('[data-cy=login-button]').click()
         cy.get('[data-cy=error-message]').should('not.be.hidden')
     });
